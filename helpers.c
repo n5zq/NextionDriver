c220c1986c41ffb5b5f7c15bb983c3e48a50e3ad
n5zq <76712226+n5zq@users.noreply.github.com>
2020-12-29 12:51:55 -0600

    Update helpers.c

--- helpers.c
+++ helpers.c
@@ -1038,3 +1038,23 @@ int openListeningSocket(void) {
     writelog(LOG_NOTICE,"Transparent Connection: listening socket open, fd=%d",display_RXsock);
     return 1;
 }
+
+char *trimwhitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
+}
