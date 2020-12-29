a24f0c79b7ece027e50d92a41bb9b44e062fd010
n5zq <76712226+n5zq@users.noreply.github.com>
2020-12-29 12:01:39 -0600

    Update helpers.c
    Added function to trim whitespace from a string

--- helpers.c
+++ helpers.c
@@ -31,6 +31,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <time.h>
+#include <ctype.h>
 
 #include "NextionDriver.h"
 #include "helpers.h"
@@ -1036,4 +1037,24 @@ int openListeningSocket(void) {
     freeaddrinfo(servinfo);
     writelog(LOG_NOTICE,"Transparent Connection: listening socket open, fd=%d",display_RXsock);
     return 1;
+}
+
+char *trimwhitespace(char *str)
+{
+  char *end;
+
+  // Trim leading space
+  while(isspace((unsigned char)*str)) str++;
+
+  if(*str == 0)  // All spaces?
+    return str;
+
+  // Trim trailing space
+  end = str + strlen(str) - 1;
+  while(end > str && isspace((unsigned char)*end)) end--;
+
+  // Write new null terminator character
+  end[1] = '\0';
+
+  return str;
 }
