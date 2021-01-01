4cbdcbce5eabb91758a07be4eec5d2b46ed29594
n5zq <76712226+n5zq@users.noreply.github.com>
2020-12-31 20:36:37 -0600

    Update helpers.c
    Added substring function

--- helpers.c
+++ helpers.c
@@ -1058,3 +1058,27 @@ char *trimwhitespace(char *str)
 
   return str;
 }
+
+char *substring(char *string, int position, int length)
+{
+   char *p;
+   int c;
+ 
+   p = malloc(length+1);
+   
+   if (p == NULL)
+   {
+      printf("Unable to allocate memory.\n");
+      exit(1);
+   }
+ 
+   for (c = 0; c < length; c++)
+   {
+      *(p+c) = *(string+position-1);      
+      string++;  
+   }
+ 
+   *(p+c) = '\0';
+ 
+   return p;
+}
